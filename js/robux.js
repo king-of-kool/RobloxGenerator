66a55de0250d55fcae41bf8e8fad568953b636c0
king-of-kool <124392143+king-of-kool@users.noreply.github.com>
2024-02-15 22:24:11 +0000

    Update robux.js

--- js/robux.js
+++ js/robux.js
@@ -1,12 +1,12 @@
-// ==UserScript==
-// @name         Add Robux on Roblox 1.2
-// @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  Robux Hack
-// @author       You
-// @match        https://www.roblox.com/*
-// @grant        none
-// ==/UserScript==
+ ==UserScript==
+ @name         Add Robux on Roblox 1.2
+ @namespace    http://tampermonkey.net/
+ @version      1.2
+ @description  Robux Hack
+ @author       You
+ @match        https://www.roblox.com/*
+ @grant        none
+ ==/UserScript==
 
 (function() {
     'use strict';
@@ -18,10 +18,10 @@ java.prototype = {script
   
 };
 
-    //robux: (1000000)function add robux()
-    //roblox: (https:roblox.com/home)
-    //add: (//1,000,000)
-    //appear: (/:1,000,000/)
+    robux: (1000000)function add robux()
+    roblox: (https:roblox.com/home)
+    add: (//1,000,000)
+    appear: (/:1,000,000/)
     function robuxadd1000000() {
   
 }
